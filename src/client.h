4fce556830043337a8e9b2845fd4ac6bcf40ff63
RadimPokorny19 <98946296+RadimPokorny@users.noreply.github.com>
2026-04-09 10:15:37 +0200

    Add cliend and server hndshk base implementation

--- src/client.h
+++ src/client.h
@@ -4,11 +4,47 @@
 
 #ifndef IPK_PROJ2_CLIENT_H
 #define IPK_PROJ2_CLIENT_H
-
+#include "udpsocket.h"
+#include "main.cpp"
+#include "rdtpacket.h"
 
 
 class client {
+public:
+    void run(Config& config) {
+        UDPSocket socket;
+        if (!socket.connect(config.address, config.port)) {
+            std:cerr << "Connection failed!" << std::endl;
+            return;
+        }
 
+        socket.setTimeout(config.timeout);
+
+        // Handshake phase
+        RDTPacket synPacket;
+        synPacket.header.conn_id = 123; // TODO: Remove placeholder
+        synPacket.header.flags = 1;
+
+        // Send a SYN and wait for SYN-ACK
+        socket.send(synPacket.serialize());
+
+        std::vector<uint8_t> response;
+
+        if (socket.receive(response) > 0) {
+            RDTPacket responsePacket;
+            if (responsePacket.deserialize(response.data(), response.size()) &&
+                (responsePacket.header.flags == 3)) {
+
+                std::cout << "Connected to the server!" << std::endl;
+
+                // Final acknowledge ACK
+                RDTPacket ackPacket;
+                ackPacket.header.conn_id = responsePacket.header.conn_id;
+                ackPacket.header.flags = 2;
+                socket.send(ackPacket.serialize());
+            }
+        }
+    }
 };
 
 
--- src/server.h
+++ src/server.h
@@ -4,11 +4,42 @@
 
 #ifndef IPK_PROJ2_SERVER_H
 #define IPK_PROJ2_SERVER_H
-
+#include <vector>
+#include "main.cpp"
+#include "rdtpacket.h"
+#include "udpsocket.h"
 
 
 class server {
+public:
+    void run(Config& config) {
+        UDPSocket socket;
+        if (!socket.bind(config.port, config.address)) {
+            std:cerr << "Failed to bind port " << config.port << std::endl;
+            return;
+        }
 
+        std::cout << "Server is listening on port " << config.port << std::endl;
+
+        while (true) {
+            std::vector<uint8_t> buffer;
+            if (socket.receive(buffer) > 0) {
+                RDTPacket incPacket;
+                if (!incPacket.deserialize(buffer.data(), buffer.size()))
+                    continue;
+
+                // Handshake procedure
+                if (incPacket.header.flags == 1) {
+                    // Receive SYN
+                    RDTPacket synACK;
+                    synACK.header.conn_id = incPacket.header.conn_id;
+                    synACK.header.flags = 3; // ACK + SYN
+                    socket.send(synACK.serialize());
+                    std::cout << "Handshake start" << std::endl;
+                }
+            }
+        }
+    }
 };
 
 
